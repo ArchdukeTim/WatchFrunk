60c01734caf6a885418e08b4500a465ad8edcfbc
Tim Winters <timwint@amazon.com>
2020-09-23 17:42:25 -0400

    Initial Commit

---
+++ WatchFrunk WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchFrunk WatchKit Extension
+//
+//  Created by Winters, Tim on 9/23/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchFrunk WatchKit Extension/WatchFrunkApp.swift
@@ -0,0 +1,19 @@
+//
+//  WatchFrunkApp.swift
+//  WatchFrunk WatchKit Extension
+//
+//  Created by Winters, Tim on 9/23/20.
+//
+
+import SwiftUI
+
+@main
+struct WatchFrunkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
